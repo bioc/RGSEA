6d8e50ebb19b4d7f4b07716d2078b5fa7e82f86b
Sonali Arora <sarora@fhcrc.org>
2014-10-07 00:15:51 +0000

    CODE UPDATE
    - add set.seed() to unitTests/test_RGSEAsd.R to generate same random numbers 
    each time the unit Test is run 
    - add RUnit to Suggests field 
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RGSEA@95036 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_RGSEAsd.R
+++ inst/unitTests/test_RGSEAsd.R
@@ -1,5 +1,10 @@
 test_RGSEAsd<-function(){
+    set.seed(1)
+    
 	data(cmap)
-	checkTrue(which.max(RGSEAsd(cmap[,1],cmap[,2:6], queryclasses=colnames(cmap)[1], refclasses=colnames(cmap)[2:6], random=5000, sd=2, iteration=100)[[1]])==3)
+	checkTrue(which.max(RGSEAsd(cmap[,1],cmap[,2:6], 
+         queryclasses=colnames(cmap)[1], 
+         refclasses=colnames(cmap)[2:6], 
+         random=5000, sd=2, iteration=100)[[1]])==3)
 }
 
